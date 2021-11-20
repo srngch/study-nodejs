f097bf8d194619161ca65e3f13b2da49ca317e0d
srngch <srngch.dev@gmail.com>
2021-11-20 16:35:27 +0900

    express

--- server.js
+++ server.js
@@ -1,15 +1,69 @@
-const http = require('http');
-const hello = require('./hello2');
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
 
-const hostname = '127.0.0.1';
-const port = 3000;
-
-const server = http.createServer((req, res) => {
-	res.statusCode = 200;
-	res.setHeader('Content-Type', 'text/plain');
-	res.end(hello.message());
+app.use((req, res, next) => {
+	console.log('Time:', Date.now());
+	next();
 });
 
-server.listen(port, hostname, () => {
-	console.log(`Server running at http://${hostname}:${port}/`);
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get('/', (req, res) => {
+	res.send('Hello World');
 });
+
+app.get('/user/:userId', (req, res) => {
+	console.log('/user:userId');
+	console.log(`${req.params.userId}의 정보를 가져옵니다`);
+
+	// TODO: DB에서 userID에 해당하는 정보를 가져오기
+	// dummy data
+	const user = {
+		userId: 13579,
+		name: 'sarchoi',
+		email: 'srngch.dev_AT_gmail.com',
+		school: '42Seoul',
+	};
+
+	res.send(user);
+});
+
+app.post('/user', (req, res) => {
+	console.log(`데이터 확인 ${req.body}`);
+
+	// TODO: DB에 데이터 저장하는 로직
+
+	res.send({state: 'OK', data: req.body});
+});
+
+app.put('/user/:userId', (req, res) => {
+	res.send('PUT (Update)');
+});
+
+app.delete('/user/:userId', (req, res) => {
+	res.send('DELETE (Delete)');
+});
+
+app.get('/user/search', (req, res) => {
+	console.log(`${req.query.name}의 정보를 조회합니다`);
+
+	// TODO: DB에서 name에 해당하는 정보를 조회하기
+	// dummy data
+	const users = [
+		{
+			userId: 13579,
+			name: 'sarchoi',
+			email: 'srngch.dev_AT_gmail.com',
+			school: '42Seoul',
+		},
+	];
+	res.send({ result: users });
+});
+
+app.listen(3000, () => {
+	console.log('Example app listening on port 3000');
+	console.log('http://localhost:3000');
+});
+
+app.use(express.static('public'));
